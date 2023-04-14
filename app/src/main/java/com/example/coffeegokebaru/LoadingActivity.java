3a04f87d2eb3ac20418800160e56e11bdaab2530
Kebaru <kebaru@yandex.ru>
2023-04-15 07:33:21 +0900

    1

---
+++ app/src/main/java/com/example/coffeegokebaru/LoadingActivity.java
@@ -0,0 +1,27 @@
+package com.example.coffeegokebaru;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
